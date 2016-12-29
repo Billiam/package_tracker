d2fba98cbb22f6b39becf43b47f26202781f5bbd
Billiam <Billiam@users.noreply.github.com>
2016-12-29 11:24:37 -0600

    Validate database pool every hour

--- config/database.rb
+++ config/database.rb
@@ -5,6 +5,7 @@ Sequel.database_timezone = :utc
 Sequel::Model.plugin(:schema)
 Sequel::Model.plugin(:timestamps)
 Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
-Sequel::Model.db =  Sequel.connect(ENV.fetch('DATABASE_URL'),  :loggers => [logger])
+Sequel::Model.db = Sequel.connect(ENV.fetch('DATABASE_URL'), :loggers => [logger])
+Sequel::Model.db.extension(:connection_validator)
 
 
