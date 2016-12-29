ae08784b28fea605483470f56df35da483f2768f
Billiam <Billiam@users.noreply.github.com>
2016-12-29 11:50:52 -0600

    More aggressive database refresh timout.

--- config/database.rb
+++ config/database.rb
@@ -7,5 +7,5 @@ Sequel::Model.plugin(:timestamps)
 Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
 Sequel::Model.db = Sequel.connect(ENV.fetch('DATABASE_URL'), :loggers => [logger])
 Sequel::Model.db.extension(:connection_validator)
-
+Sequel::Model.db.pool.connection_validation_timeout = 300
 
