57f401e5d5d540e0fbd13e1606365837dddd7ea0
Billiam <Billiam@users.noreply.github.com>
2016-12-28 21:10:47 -0600

    Match ups ship notifications by subject

--- lib/email_tracker/ups.rb
+++ lib/email_tracker/ups.rb
@@ -3,6 +3,9 @@ module EmailTracker::Ups
     return unless message.from.any? {|from| from.host == 'ups.com' && from.mailbox == 'mcinfo' }
 
     match = /\*Tracking Number:\* ([a-zA-Z0-9]{10}\d{8})\b/.match(message.body)
+    return [:ups, match[1]] if match
+
+    match = /Tracking Number ([a-zA-Z0-9]{10}\d{8})\b/.match(message.subject)
     [:ups, match[1]] if match
   end
 end
