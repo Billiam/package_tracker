8d22188a22d5c3c10ceb66a87f23372682de1a0e
Billiam <Billiam@users.noreply.github.com>
2017-01-01 18:52:40 -0600

    Add dirty model storage.

--- config/database.rb
+++ config/database.rb
@@ -4,9 +4,9 @@ Sequel.database_timezone = :utc
 
 Sequel::Model.plugin(:schema)
 Sequel::Model.plugin(:timestamps)
+Sequel::Model.plugin(:dirty)
 Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
 Sequel::Model.db = Sequel.connect(ENV.fetch('DATABASE_URL'), :loggers => [logger])
 Sequel::Model.db.extension(:connection_validator)
 Sequel::Model.db.extension(:pg_json)
 Sequel::Model.db.pool.connection_validation_timeout = 300
-
