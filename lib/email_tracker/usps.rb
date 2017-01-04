724c8c560a4573fa558a92e7a7dc02abdb70b711
Billiam <Billiam@users.noreply.github.com>
2017-01-04 12:34:43 -0600

    Expand tracking number range for usps.

--- lib/email_tracker/usps.rb
+++ lib/email_tracker/usps.rb
@@ -2,7 +2,7 @@ module EmailTracker::Usps
   def self.tracking_data(message)
     return unless message.from.any? {|from| from.host == 'usps.com' }
 
-    match = /Tracking Number: (\d{22})\b/.match(message.body)
+    match = /Tracking Number: (\d{20,26})\b/.match(message.body)
     [:usps, match[1]] if match
   end
 end
