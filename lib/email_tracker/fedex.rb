70cea211016e0362baa2b99829bbb6de40b2723a
Billiam <Billiam@users.noreply.github.com>
2017-01-04 12:38:37 -0600

    Fix fedex email address check.

--- lib/email_tracker/fedex.rb
+++ lib/email_tracker/fedex.rb
@@ -1,6 +1,6 @@
 module EmailTracker::Fedex
   def self.tracking_data(message)
-    return unless message.from.any? { |from| from.host == 'fedex.com.com' && from.mailbox == 'TrackingUpdates' }
+    return unless message.from.any? { |from| from.host == 'fedex.com' && from.mailbox == 'TrackingUpdates' }
 
     match = /\*Tracking number:\* (\d{12,22})\b/.match(message.body)
     match = /tracknumbers=(\d{12,22})/.match(message.body) unless match
