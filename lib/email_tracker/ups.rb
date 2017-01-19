9415dce56414294e4ba3ff65327e93703762247e
Billiam <billiamthesecond@gmail.com>
2017-01-19 11:23:52 -0600

    Allow encoded characters when parsing ups email

--- lib/email_tracker/ups.rb
+++ lib/email_tracker/ups.rb
@@ -5,7 +5,7 @@ module EmailTracker::Ups
     match = /\*Tracking Number:\* ([a-zA-Z0-9]{10}\d{8})\b/.match(message.body)
     return [:ups, match[1]] if match
 
-    match = /Tracking Number ([a-zA-Z0-9]{10}\d{8})\b/.match(message.subject)
+    match = /Tracking Number(?: |=C2=AO)([a-zA-Z0-9]{10}\d{8})\b/.match(message.subject)
     [:ups, match[1]] if match
   end
 end
