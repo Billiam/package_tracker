c3d9c115e2b5605e761ec2b99793cb4f691a6d88
Billiam <billiamthesecond@gmail.com>
2017-01-19 12:15:28 -0600

    Split up ups regular expressions.

--- lib/email_tracker/ups.rb
+++ lib/email_tracker/ups.rb
@@ -1,12 +1,30 @@
 module EmailTracker::Ups
+  BODY_EXPRESSIONS = [
+    /\*Tracking Number:\* ([a-zA-Z0-9]{10}\d{8})\b/,
+    /Tracking Number(?:\s|=C2=AO)([a-zA-Z0-9]{10}\d{8})\b/
+  ]
+  SUBJECT_EXPRESSIONS = [
+    /Tracking Number ([a-zA-Z0-9]{10}\d{8})\b/
+  ]
+
   def self.tracking_data(message)
     return unless message.from.any? {|from| from.host == 'ups.com' && from.mailbox == 'mcinfo' }
 
-    match = /\*Tracking Number:\* ([a-zA-Z0-9]{10}\d{8})\b/.match(message.body)
-    return [:ups, match[1]] if match
+    tracking_number = self.find_tracking_number(message)
+    [:ups, tracking_number] if tracking_number
+  end
 
-    match = /Tracking Number(?: |=C2=AO)([a-zA-Z0-9]{10}\d{8})\b/.match(message.subject)
-    [:ups, match[1]] if match
+  private
+
+  def self.find_tracking_number(message)
+    result = BODY_EXPRESSIONS.detect do |expression|
+      message.body[expression, 1]
+    end
+    return result if result
+
+    SUBJECT_EXPRESSIONS.detect do |expression|
+      message.subject[expression, 1]
+    end
   end
 end
 
