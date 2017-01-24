f1d22243701f4426b1bfa2c731dcd78b3895e085
Billiam <billiamthesecond@gmail.com>
2017-01-24 11:30:40 -0600

    Match on fedex shipment subject.

--- lib/email_tracker/fedex.rb
+++ lib/email_tracker/fedex.rb
@@ -3,7 +3,8 @@ module EmailTracker::Fedex
     return unless message.from.any? { |from| from.host == 'fedex.com' && from.mailbox == 'TrackingUpdates' }
 
     match = /\*Tracking number:\* (\d{12,22})\b/.match(message.body)
-    match = /tracknumbers=(\d{12,22})/.match(message.body) unless match
+    match = /tracknumbers=(\d{12,22})\b/.match(message.body) unless match
+    match = /FedEx Shipment (\d{12,22})\b/.match(message.subject) unless match
 
     [:fedex, match[1]] if match
   end
